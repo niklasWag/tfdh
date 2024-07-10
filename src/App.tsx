52a06d2c5bce535a8044f647fed8780119ee9a0b
Niklas Wagner <niklas.wagner@sap.com>
2024-07-10 09:19:20 +0200

    🎨 move mantine provider to root

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import '@mantine/core/styles.css'
 import './App.css'
 import { HeaderMenu } from './components/header/Header'
 import icon from './assets/images/android-chrome-512x512.png'
 
-import { MantineProvider, Button, Image } from '@mantine/core'
+import { Button, Image } from '@mantine/core'
 
 const maintenance = true
 
@@ -14,7 +13,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   if (maintenance) {
-    return (<MantineProvider defaultColorScheme='dark'>{
+    return (
       <>
         <div style={{display: 'flex', flexDirection: 'column'}}>
           <Image
@@ -26,38 +25,36 @@ function App() {
           <h1>Maintenance</h1>
         </div>
       </>
-    }</MantineProvider>)
+    )
   }
 
   return (
-    <MantineProvider defaultColorScheme='dark'>{
-      <>
-        <div className='container'>
-          <HeaderMenu />
-          <div>
-            <a href="https://vitejs.dev" target="_blank">
-              <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-          </div>
-          <h1>Vite + React</h1>
-          <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-            </button>
-            <Button onClick={() => alert()}>TEST</Button>
-            <p>
-              Edit <code>src/App.tsx</code> and save to test HMR
-            </p>
-          </div>
-          <p className="read-the-docs">
-            Click on the Vite and React logos to learn more
+    <>
+      <div className='container'>
+        <HeaderMenu />
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <Button onClick={() => alert()}>TEST</Button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
           </p>
         </div>
-      </>
-    }</MantineProvider>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </>
   )
 }
 
